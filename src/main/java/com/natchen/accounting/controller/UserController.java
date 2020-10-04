f91dba92363293b535273da8aac5e5b2cb538467
Nat Chen <chen_natalie@outlook.com>
2020-10-04 12:13:52 +0800

    feat(exception-handler): Implement multiple exception handler

--- src/main/java/com/natchen/accounting/controller/UserController.java
+++ src/main/java/com/natchen/accounting/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.natchen.accounting.controller;
 
 import com.natchen.accounting.converter.c2s.UserInfoC2SConverter;
+import com.natchen.accounting.exception.InvalidParameterException;
 import com.natchen.accounting.manager.UserInfoManager;
 import com.natchen.accounting.model.service.UserInfo;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,10 +29,12 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public UserInfo getUserInfoByUserId(@PathVariable("id") Long userId) {
+    public ResponseEntity<UserInfo> getUserInfoByUserId(@PathVariable("id") Long userId) {
         log.info("Get user info by user id {}", userId);
+        if (userId == null || userId <= 0L) {
+            throw new InvalidParameterException(String.format("The user id %s is invalid", userId));
+        }
         val userInfo = userInfoManager.getUserInfoByUserId(userId);
-        return userInfoC2SConverter.convert(userInfo);
-
+        return ResponseEntity.ok(userInfoC2SConverter.convert(userInfo));
     }
 }
--- src/main/java/com/natchen/accounting/manager/UserInfoManagerImpl.java
+++ src/main/java/com/natchen/accounting/manager/UserInfoManagerImpl.java
@@ -3,10 +3,13 @@ package com.natchen.accounting.manager;
 
 import com.natchen.accounting.converter.p2c.UserInfoP2CConverter;
 import com.natchen.accounting.dao.UserInfoDAO;
+import com.natchen.accounting.exception.ResourceNotFoundException;
 import com.natchen.accounting.model.common.UserInfo;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class UserInfoManagerImpl implements UserInfoManager {
@@ -23,7 +26,8 @@ public class UserInfoManagerImpl implements UserInfoManager {
 
     @Override
     public UserInfo getUserInfoByUserId(Long userId) {
-        val userInfo = userInfoDAO.getUserInfoById(userId);
+        val userInfo = Optional.ofNullable(userInfoDAO.getUserInfoById(userId))
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("User %s was not found", userId)));
         return userInfoP2CConverter.convert(userInfo);
     }
 }
